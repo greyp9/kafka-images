965634b7a66a448d043a7f4e1254d7edaa344eed
Paul Grey <greyp9@yahoo.com>
2024-12-30 15:31:46 -0500

    sasl 2412

--- examples/kafka-cluster-sasl/secrets/create-certs.sh
+++ examples/kafka-cluster-sasl/secrets/create-certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm ssl/*.csr ssl/*.jks ssl/*.key ssl/*.srl ssl/*.crt ssl/*creds ssl/*.req ssl/*.pem
+rm secrets/ssl/*.csr secrets/ssl/*.jks secrets/ssl/*.key secrets/ssl/*.srl secrets/ssl/*.crt secrets/ssl/*creds secrets/ssl/*.req secrets/ssl/*.pem
 
 set -o nounset \
     -o errexit \
@@ -8,40 +8,40 @@ set -o nounset \
     -o xtrace
 
 # Generate CA key
-openssl req -new -x509 -keyout ssl/snakeoil-ca-1.key -out ssl/snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
+openssl req -new -x509 -keyout secrets/ssl/snakeoil-ca-1.key -out secrets/ssl/snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
 # Kafkacat
-openssl genrsa -des3 -passout "pass:confluent" -out ssl/kafkacat.client.key 1024
-openssl req -passin "pass:confluent" -passout "pass:confluent" -key ssl/kafkacat.client.key -new -out ssl/kafkacat.client.req -subj '/CN=kafkacat.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US'
-openssl x509 -req -CA ssl/snakeoil-ca-1.crt -CAkey ssl/snakeoil-ca-1.key -in ssl/kafkacat.client.req -out ssl/kafkacat-ca1-signed.pem -days 9999 -CAcreateserial -passin "pass:confluent"
+openssl genrsa -des3 -passout "pass:confluent" -out secrets/ssl/kafkacat.client.key 1024
+openssl req -passin "pass:confluent" -passout "pass:confluent" -key secrets/ssl/kafkacat.client.key -new -out secrets/ssl/kafkacat.client.req -subj '/CN=kafkacat.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US'
+openssl x509 -req -CA secrets/ssl/snakeoil-ca-1.crt -CAkey secrets/ssl/snakeoil-ca-1.key -in secrets/ssl/kafkacat.client.req -out secrets/ssl/kafkacat-ca1-signed.pem -days 9999 -CAcreateserial -passin "pass:confluent"
 
 
 # kafka-sasl-1 kafka-sasl-2 kafka-sasl-3
-for i in kafka-sasl-lo producer consumer
+for i in kafka-sasl-1.eden kafka-sasl-2.eden kafka-sasl-3.eden producer consumer
 do
 	echo $i
 	# Create keystores
 	keytool -genkey -noprompt \
 				 -alias $i \
 				 -dname "CN=$i, OU=TEST, O=CONFLUENT, L=PaloAlto, S=Ca, C=US" \
-				 -keystore ssl/kafka.$i.keystore.jks \
+				 -keystore secrets/ssl/kafka.$i.keystore.jks \
 				 -keyalg RSA \
 				 -storepass confluent \
 				 -keypass confluent
 
 	# Create CSR, sign the key and import back into keystore
-	keytool -keystore ssl/kafka.$i.keystore.jks -alias $i -certreq -file ssl/$i.csr -storepass confluent -keypass confluent
+	keytool -keystore secrets/ssl/kafka.$i.keystore.jks -alias $i -certreq -file secrets/ssl/$i.csr -storepass confluent -keypass confluent
 
-	openssl x509 -req -CA ssl/snakeoil-ca-1.crt -CAkey ssl/snakeoil-ca-1.key -in ssl/$i.csr -out ssl/$i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent
+	openssl x509 -req -CA secrets/ssl/snakeoil-ca-1.crt -CAkey secrets/ssl/snakeoil-ca-1.key -in secrets/ssl/$i.csr -out secrets/ssl/$i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent
 
-	keytool -noprompt -keystore ssl/kafka.$i.keystore.jks -alias CARoot -import -file ssl/snakeoil-ca-1.crt -storepass confluent -keypass confluent
+	keytool -noprompt -keystore secrets/ssl/kafka.$i.keystore.jks -alias CARoot -import -file secrets/ssl/snakeoil-ca-1.crt -storepass confluent -keypass confluent
 
-	keytool -noprompt -keystore ssl/kafka.$i.keystore.jks -alias $i -import -file ssl/$i-ca1-signed.crt -storepass confluent -keypass confluent
+	keytool -noprompt -keystore secrets/ssl/kafka.$i.keystore.jks -alias $i -import -file secrets/ssl/$i-ca1-signed.crt -storepass confluent -keypass confluent
 
 	# Create truststore and import the CA cert.
-	keytool -noprompt -keystore ssl/kafka.$i.truststore.jks -alias CARoot -import -file ssl/snakeoil-ca-1.crt -storepass confluent -keypass confluent
+	keytool -noprompt -keystore secrets/ssl/kafka.$i.truststore.jks -alias CARoot -import -file secrets/ssl/snakeoil-ca-1.crt -storepass confluent -keypass confluent
 
-  echo "confluent" > ssl/${i}_sslkey_creds
-  echo "confluent" > ssl/${i}_keystore_creds
-  echo "confluent" > ssl/${i}_truststore_creds
+  echo "confluent" > secrets/ssl/${i}_sslkey_creds
+  echo "confluent" > secrets/ssl/${i}_keystore_creds
+  echo "confluent" > secrets/ssl/${i}_truststore_creds
 done
--- examples/kafka-cluster-sasl/secrets/create-keytabs.sh
+++ examples/kafka-cluster-sasl/secrets/create-keytabs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm sasl/*.keytab
+rm secrets/sasl/*.keytab
 
 export ZK_KEYTAB_HOSTNAME=localhost
 export KEYTAB_HOSTNAME_LO=kafka-sasl-lo
 export KEYTAB_HOSTNAME_EN0=kafka-sasl-en0
 
-for principal in zookeeper
+for ordinal in "1" "2" "3"
 do
-  docker exec -it kerberos-kerberos-1 kadmin.local -q "addprinc -randkey zookeeper/${ZK_KEYTAB_HOSTNAME}@TEST.CONFLUENT.IO"
-  docker exec -it kerberos-kerberos-1 kadmin.local -q "ktadd -norandkey -k /tmp/keytab/sasl/${principal}.keytab zookeeper/localhost@TEST.CONFLUENT.IO"
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "addprinc -randkey zookeeper/zk-sasl-${ordinal}.eden@TEST.CONFLUENT.IO"
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "ktadd -norandkey -k /tmp/keytab/sasl/zk${ordinal}.keytab zookeeper/zk-sasl-${ordinal}.eden@TEST.CONFLUENT.IO"
 done
 
 #for principal in zookeeper1 zookeeper2 zookeeper3
@@ -18,16 +18,19 @@ done
 #  docker exec -it kerberos kadmin.local -q "ktadd -norandkey -k /tmp/keytab/${principal}.keytab zookeeper/${KEYTAB_HOSTNAME}@TEST.CONFLUENT.IO"
 #done
 
-for principal in zkclient
+for ordinal in "1" "2" "3"
 do
-  docker exec -it kerberos-kerberos-1 kadmin.local -q "addprinc -randkey zkclient/${KEYTAB_HOSTNAME_LO}@TEST.CONFLUENT.IO"
-  docker exec -it kerberos-kerberos-1 kadmin.local -q "ktadd -norandkey -k /tmp/keytab/sasl/${principal}.keytab zkclient/${KEYTAB_HOSTNAME_LO}@TEST.CONFLUENT.IO"
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "addprinc -randkey zkclient/zk-sasl-${ordinal}@TEST.CONFLUENT.IO"
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "ktadd -norandkey -k /tmp/keytab/sasl/zkclient${ordinal}.keytab zkclient/zk-sasl-${ordinal}@TEST.CONFLUENT.IO"
 done
 
-for principal in broker1 broker2 broker3
+for ordinal in "1" "2" "3"
 do
-  docker exec -it kerberos-kerberos-1 kadmin.local -q "addprinc -randkey kafka/${KEYTAB_HOSTNAME_LO}@TEST.CONFLUENT.IO"
-  docker exec -it kerberos-kerberos-1 kadmin.local -q "ktadd -norandkey -k /tmp/keytab/sasl/${principal}.keytab kafka/${KEYTAB_HOSTNAME_LO}@TEST.CONFLUENT.IO"
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "addprinc -randkey kafka/kafka-sasl-${ordinal}@TEST.CONFLUENT.IO"
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "ktadd -norandkey -k /tmp/keytab/sasl/broker${ordinal}c.keytab kafka/kafka-sasl-${ordinal}@TEST.CONFLUENT.IO"
+
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "addprinc -randkey kafka/kafka-sasl-${ordinal}.eden@TEST.CONFLUENT.IO"
+  docker exec -it kerberos-kerberos-1 kadmin.local -q "ktadd -norandkey -k /tmp/keytab/sasl/broker${ordinal}s.keytab kafka/kafka-sasl-${ordinal}.eden@TEST.CONFLUENT.IO"
 done
 
 for principal in producer1 consumer1
